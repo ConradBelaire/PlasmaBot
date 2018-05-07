4a15e221c6b89c8ee7ce7f45f48853c58bbede68
ConradBelaire <cbelaire@gmail.com>
2018-05-07 12:29:35 -0400

    Module time
    Moved square into its own module
    Changed "client" to "bot", more in line with other bots I see
    Added more credits to readme

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # PlasmaBot
-Lovely little discord bot, mostly made for the purpose of learning
+Lovely little discord bot, mostly made for the purpose of learning.
+
+
+
+Big thanks to:
+
+[Dev Dungeon](https://www.devdungeon.com/content/make-discord-bot-python) for the tutorial, I would not have been able to make this bot without it.
+
+[Dataquest.io](https://www.dataquest.io/blog/python-api-tutorial/) for teaching me how to use APIs.
+
+[leovoel's basic_bot.py](https://gist.github.com/leovoel/46cd89ed6a8f41fd09c5) for using extensions.
--- main.py
+++ main.py
@@ -5,15 +5,18 @@ import aiohttp
 from discord import Game
 from discord.ext.commands import Bot
 
-BOT_PREFIX = ("``")
+BOT_PREFIX = ("``", "@PlasmaBot#0176")
 TOKEN = "NDQyNzkwNTMxODE5NjM0Njg4.DdEQig.5tiD4083_DVfP_cfzXzeR6k-TKI"  # Get at discordapp.com/developers/applications/me
 DESCRIPTION = '''A bot with just a bunch of random stuff.
 
 PlasmaLink's baby.'''
 
-client = Bot(command_prefix=BOT_PREFIX, description=DESCRIPTION)
+# this specifies what extensions to load when the bot starts up
+startup_extensions = ["Mathstuff"]
 
-@client.command(name='8ball',
+bot = Bot(command_prefix=BOT_PREFIX, description=DESCRIPTION)
+
+@bot.command(name='8ball',
                 description="Answers a yes/no question.",
                 brief="Answers from the beyond.",
                 aliases=['eight_ball', 'eightball', '8-ball'],
@@ -26,15 +29,11 @@ async def eight_ball(context):
         'owo',
         'yup yup yup'
     ]
-    await client.say(random.choice(possible_responses) + ", " + context.message.author.mention)
+    await bot.say(random.choice(possible_responses) + ", " + context.message.author.mention)
 
-@client.command()
-async def square(number):
-	squared_value = int(number) * int(number)
-	await client.say(str(number) + "^2 = " + str(squared_value))
 	
 	
-@client.command(name="roll",
+@bot.command(name="roll",
 				description="Roll a dice with specified sides",
 				brief="Roll a die!",
 				aliases=['r', 'dice'])
@@ -42,11 +41,11 @@ async def roll(number):
 	try:
 		d = int(number)
 		r = random.randrange(1, d+1)
-		await client.say("Rolling a d" + number + "... It returned a " + str(r) + "!")
+		await bot.say("Rolling a d" + number + "... It returned a " + str(r) + "!")
 	except ValueError:
-		await client.say("That's not a valid number!")
+		await bot.say("That's not a valid number!")
 
-@client.command()
+@bot.command()
 async def ISS():
 	# Set up the parameters we want to pass to the API.
 	# This is the latitude and longitude of New York City.
@@ -60,28 +59,35 @@ async def ISS():
 	print(type(data))
 	print(data)
 	
-@client.command(name="astros",
+@bot.command(name="astros",
 				description="Displays how many people are currently in space.",
 				brief="How many are in space?")
 async def astros():
 	response = requests.get("http://api.open-notify.org/astros.json")
 	data = response.json()
-	await client.say("There are currently " + str(data["number"]) + " people in space right now!")
+	await bot.say("There are currently " + str(data["number"]) + " people in space right now!")
 	
 
 async def list_servers():
-	await client.wait_until_ready()
-	while not client.is_closed:
+	await bot.wait_until_ready()
+	while not bot.is_closed:
 		print("Current servers:")
-		for server in client.servers:
+		for server in bot.servers:
 			print(server.name)
 		await asyncio.sleep(600)
 	
-@client.event
+@bot.event
 async def on_ready():
-    await client.change_presence(game=Game(name="big mood"))
-    print("Logged in as " + client.user.name)
+	await bot.change_presence(game=Game(name="big mood"))
+	print("Logged in as " + bot.user.name)
+	
+	for extension in startup_extensions:
+		try:
+			bot.load_extension(extension)
+		except Exception as e:
+			exc = '{}: {}'.format(type(e).__name__, e)
+			print('Failed to load extension {}\n{}'.format(extension, exc))
 	
 	
-client.loop.create_task(list_servers())
-client.run(TOKEN)
+bot.loop.create_task(list_servers())
+bot.run(TOKEN)
